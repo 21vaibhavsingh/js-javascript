937d6787a8afc77d4b6c645f7b2c6f73e8cc69ff
vaibhav pratap singh <21vaibhavps@gmail.com>
2025-06-01 23:33:33 +0530

    Successfully uploaded !

---
+++ lec5str.js
@@ -0,0 +1,24 @@
+const name = "vaibahv"
+const lname = "singh"
+
+ // console.log(name+lname);  // old syntax
+
+ console.log(`hello my name is ${name} and surname is ${lname}`);
+
+ const gamename = new String('vaibhav')
+ console.log(gamename[0])
+ console.log(gamename.__proto__);
+ console.log(gamename.length);
+ console.log(gamename.toUpperCase());
+ console.log(gamename.charAt(2));
+ console.log(gamenname.indexOf('b'))
+
+ const newSting = gamename.substring(0,5);
+ console.log(substring)
+
+ const anotherString = gamename.slice(-8,4)
+ console.log(anotherString)
+
+ const newstring ="   vaibhav   "
+console.log(newstring)
+console.log(newstring.trim());
