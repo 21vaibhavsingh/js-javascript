60a3973577f2b5b241660a88595945307daeb881
vaibhav pratap singh <21vaibhavps@gmail.com>
2025-06-01 23:32:14 +0530

    Successfully uploaded !

---
+++ lec1.js
@@ -0,0 +1,29 @@
+//console.log("vaibhav") 
+                        // screen pe print karado if ci=onsole log me value mile toh
+
+const accountID = 144553
+                        // const = change nhi hoga , valur lock hogai
+let accountEmail = "abc@gmail.com"
+                        //
+var accountPswd = "12345"
+accountCity = "jaipur"
+
+//accountID = 2   {not possible to change const value}
+//console.log(accountID);
+
+accountEmail = "xyz@gmail.com"
+accountPswd = "212121"
+accountCity = "asdfg"
+//console.log(accountEmail);
+//console.log(accountpswd);
+//console.log(accountCity);
+let accountState;
+
+console.log([accountID, accountEmail,accountPswd,accountCity,accountState])
+        // var and let used to declare variable;
+        //scope problem solved by let
+
+/* 
+prefer not to use var
+because of issue in block scope and functional scope
+*/
